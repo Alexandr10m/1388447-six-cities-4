f1a8680a5495b4299d3c932cc14da947efd99017
Alexandr10m <serbackij@gmail.com>
2020-06-18 20:28:49 +0300

    Отредактировал компонент Card и добавил тестов

--- src/components/card/card.e2e.test.js
+++ src/components/card/card.e2e.test.js
@@ -8,15 +8,24 @@ Enzyme.configure({
   adapter: new Adapter()
 });
 
+const offer = {
+  isPremium: true,
+  picture: `picture`,
+  price: 100,
+  isFavourite: false,
+  grade: 4,
+  title: `title`,
+  type: `Hotel`,
+};
+
 describe(`E2E test of Card`, () => {
   it(`CardComponent title should pressed one time`, () => {
     const onCardTitleClick = jest.fn();
-
     const cardComponent = shallow(
         < Card
-          offerName={`Wood and stone place`}
-          grade={80}
+          offer={offer}
           onCardTitleClick={onCardTitleClick}
+          onActiveCard={() => {}}
         />
     );
 
@@ -24,5 +33,22 @@ describe(`E2E test of Card`, () => {
     cardTitle.simulate(`click`);
 
     expect(onCardTitleClick.mock.calls.length).toBe(1);
+  });
+
+  it(`Cursor hovering over the card by the user returns corresponding object`, () => {
+    const onActiveCard = jest.fn((arg) => arg);
+    const cardComponent = shallow(
+        < Card
+          offer={offer}
+          onCardTitleClick={() => {}}
+          onActiveCard={onActiveCard}
+        />
+    );
+
+    const card = cardComponent.find(`article.place-card`);
+    card.simulate(`mouseenter`);
+
+    expect(onActiveCard.mock.calls.length).toBe(1);
+    expect(onActiveCard.mock.calls[0][0]).toMatchObject(offer);
   });
 });
