68b80cd10d272df36bf53f26a777f11ebf5ea71e
Alexandr10m <serbackij@gmail.com>
2020-06-25 14:56:27 +0300

    Тест Map

---
+++ src/components/map/map.test.js
@@ -0,0 +1,68 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Map from "./map.jsx";
+
+
+const offerWithPremium = {
+  isPremium: true,
+  pictures: [`picture`],
+  price: 100,
+  isFavourite: false,
+  grade: 4,
+  title: `title`,
+  type: `Hotel`,
+  coords: [52.3909553943508, 4.85309666406198],
+};
+
+const offerWithoutPremium = {
+  isPremium: true,
+  pictures: [`picture`],
+  price: 100,
+  isFavourite: false,
+  grade: 4,
+  title: `title`,
+  type: `Hotel`,
+  coords: [52.3909553943508, 4.929309666406198],
+};
+
+const offerWithFavourite = {
+  isPremium: true,
+  pictures: [`picture`],
+  price: 100,
+  isFavourite: true,
+  grade: 4,
+  title: `title`,
+  type: `Hotel`,
+  coords: [52.369553943508, 4.85309666406198],
+};
+
+const offerWithoutFavourite = {
+  isPremium: true,
+  pictures: [`picture`],
+  price: 100,
+  isFavourite: false,
+  grade: 4,
+  title: `title`,
+  type: `Hotel`,
+  coords: [52.3909553943508, 4.85309666406198],
+};
+
+const offers = [
+  offerWithPremium,
+  offerWithoutPremium,
+  offerWithFavourite,
+  offerWithoutFavourite
+];
+
+describe(`Snapshot of Map`, () => {
+  it(`Map should render correctly`, () => {
+    const tree = renderer
+      .create(
+          <Map
+            offers={offers}
+          />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
