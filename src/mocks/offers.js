bc340b350d64ab4209b009ef1d7faeff4335bd4d
Alexandr10m <serbackij@gmail.com>
2020-06-25 19:53:37 +0300

    В мокковые данные добавил поля

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -46,6 +46,13 @@ const createOffers = (count) => {
       maxAdults: getRandomIntegerNumber(1, 10),
       facilities: getRandomArrayLength(FACILITIES),
       coords: COORDS[i],
+      id: new Date() + i,
+      reviews: [{
+        image: `img/avatar-max.jpg`,
+        text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+        name: `Max`,
+        time: `April 2019`,
+      }],
     });
   }
 
