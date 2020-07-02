65669c4e00d38523c2bf03d3dc96a51a44e1beda
Alexandr10m <serbackij@gmail.com>
2020-07-02 12:37:35 +0300

    Добавил функцию в utils и названия сортировок в constants

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,19 @@
 const firstWordInUpper = (str) => {
   const isString = typeof str === `string` || !!str;
-
   return isString ? str[0].toUpperCase() + str.slice(1) : str;
+};
+
+const getOptionValue = (str) => {
+  const slices = str.toLowerCase().split(` `);
+
+  if (slices.length === 1) {
+    return slices[0];
+  }
+  if (slices.length === 3) {
+    return `${slices[0]}-${slices[1]}`;
+  }
+
+  return `${slices[2]}-${slices[3]}`;
 };
 
 const rating = (grade) => ({width: `${grade * 20}%`});
@@ -10,5 +22,26 @@ const extend = (a, b) => {
   return Object.assign({}, a, b);
 };
 
+const sortByType = (type, array) => {
+  const copyArray = [...array];
 
-export {firstWordInUpper, rating, extend};
+  switch (type) {
+    case `Popular`:
+      break;
+
+    case `Price: low to high`:
+      copyArray.sort((prevOffer, nextOffer) => prevOffer.price - nextOffer.price);
+      break;
+    case `Price: high to low`:
+      copyArray.sort((prevOffer, nextOffer) => nextOffer.price - prevOffer.price);
+      break;
+    case `Top rated first`:
+      copyArray.sort((prevOffer, nextOffer) => nextOffer.grade - prevOffer.grade);
+      break;
+  }
+
+  return copyArray;
+};
+
+
+export {firstWordInUpper, rating, extend, sortByType, getOptionValue};
