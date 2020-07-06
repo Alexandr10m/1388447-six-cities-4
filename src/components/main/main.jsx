1b5a8029f1d09cc26c2f9a4f5d920d4e0710c1f4
Alexandr10m <serbackij@gmail.com>
2020-07-06 17:11:24 +0300

    Добавил новые компоненты MainEmpty и MainOffers

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,53 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import ListCards from "../list-cards/list-cards.jsx";
-import Map from "../map/map.jsx";
 import ListCities from "../list-cities/list-cities.jsx";
-import Sort from "../sort/sort.jsx";
+import MainEmpty from "../main-empty/main-empty.jsx";
+import MainOffers from "../main-offers/main-offers.jsx";
 
 
 const Main = (props) => {
   const {city, offers, onCardTitleClick, onCityClick} = props;
-  const localOffers = offers.localOffers;
-  const cityCoords = offers.cityCoords;
+  const isEmptyOffers = !!offers.localOffers.length;
+  const EmptyMainClass = isEmptyOffers ? `page__main--index-empty` : ``;
 
   return (
-    <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
-      <div className="tabs">
-        <section className="locations container">
+    <div className="page page--gray page--main">
+      <main className={`page__main page__main--index ${EmptyMainClass}`}>
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
 
-          <ListCities
-            onCityClick={onCityClick}
-            currentCity={city}
-          />
-
-        </section>
-      </div>
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{offers.localOffers.length} places to stay in {city}</b>
-            <Sort/>
-            <ListCards
-              onCardTitleClick={onCardTitleClick}
+            <ListCities
+              onCityClick={onCityClick}
+              currentCity={city}
             />
 
           </section>
-          <div className="cities__right-section">
-            <section className="cities__map map">
-
-              <Map
-                localOffers={localOffers}
-                city={cityCoords}
-              />
-
-            </section>
-          </div>
         </div>
-      </div>
-    </main>
+        {isEmptyOffers ? <MainOffers city={city} offers={offers} onCardTitleClick={onCardTitleClick} onCityClick={onCityClick}/> : <MainEmpty city={city}/>}
+      </main>
+    </div>
   );
 };
 
