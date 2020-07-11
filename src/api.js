7ee012dc5da7bca5e1d9fb339f49ede668d0d2a9
Alexandr10m <serbackij@gmail.com>
2020-07-11 18:21:09 +0300

    Добавил в api обработку ошибки по авторизации пользователя

--- src/api.js
+++ src/api.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 
 
-const createAPI = () => {
+const ErrorCode = {
+  UNAUTHORIZED: 401,
+  BAD_REQUEST: 400,
+};
+
+const createAPI = (onUnauthorized) => {
   const api = axios.create({
     baseURL: `https://4.react.pages.academy/six-cities`,
     timeout: 5000,
     withCredentials: true,
   });
+
+  const onSuccess = (response) => response;
+
+  const onFail = (err) => {
+    const {response} = err;
+    const isAuthError = response.status === ErrorCode.UNAUTHORIZED || response.status === ErrorCode.BAD_REQUEST;
+
+    if (isAuthError) {
+      onUnauthorized();
+      throw err;
+    }
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
 
   return api;
 };
