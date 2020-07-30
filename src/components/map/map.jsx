500ec313bfb1888d7a2dfeb7ecdeb89c69ffd1ae
Alexandr10m <serbackij@gmail.com>
2020-07-30 12:01:13 +0300

    Почистил за собой мусор в Map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -50,9 +50,6 @@ class Map extends PureComponent {
 
   _addMarkers() {
     const {localOffers, activeCard} = this.props;
-    console.log(`localOffers:`, localOffers);
-    console.log(`activeCard`, activeCard);
-
 
     this.layerGroup = LeafLet.layerGroup().addTo(this._map);
 
@@ -61,7 +58,6 @@ class Map extends PureComponent {
         if (offer.id === activeCard.id) {
           LeafLet.marker(offer.coords, {icon: this._activeIcon})
             .addTo(this.layerGroup);
-          console.log(activeCard, `active`);
         } else {
           LeafLet.marker(offer.coords, {icon: this._icon})
             .addTo(this.layerGroup);
