49d1795252f17e5152fe2bc0676c38b149102c46
Alexandr10m <serbackij@gmail.com>
2020-08-06 16:53:01 +0300

    Добавил новый компонент NetworkError

---
+++ src/components/network-error/network-error.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {NetworkError} from "./network-error.jsx";
+import configureStore from "redux-mock-store";
+import {Provider} from "react-redux";
+import NameSpace from "../../reducer/name-space.js";
+
+
+const mockStore = configureStore([]);
+
+describe(`Snapshot of NetworkError`, () => {
+  it(`NetworkError should render correct`, () => {
+    const store = mockStore({
+      [NameSpace.STATE]: {},
+      [NameSpace.DATA]: {
+        textError: `some error`,
+      },
+      [NameSpace.USER]: {},
+    });
+
+    const tree = renderer
+      .create(
+          <Provider store={store}>
+            <NetworkError
+              textError={`some error`}
+            />
+          </Provider>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
